1eaf562a9f737cc0e15f81ec08f25259eedd2e7c
doggbmx <manuelorticosta@gmail.com>
2023-07-18 22:16:24 -0400

    end class 5

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener("load", startGame);
+window.addEventListener("load", renderCanvas);
 window.addEventListener("resize", setCanvasSize);
 
 // global variables
@@ -27,9 +27,16 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
+  // THIS WAS A TOUGH ONE
   for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis["X"], elementsSize * i, elementsSize * i);
+    // game.fillText(emojis["X"], elementsSize * i, elementsSize * i);
+    for (let j = 1; j <= 10; j++) {
+      game.fillText(emojis["X"], elementsSize * i + 5, elementsSize * j);
+    }
   }
 }
 
-function renderCanvas() {}
+function renderCanvas() {
+  setCanvasSize();
+  startGame();
+}
