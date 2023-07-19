812d5bacdb0d726532424e2ca7c4beba477fa4df
doggbmx <manuelorticosta@gmail.com>
2023-07-18 21:59:54 -0400

    end class 3

--- game.js
+++ game.js
@@ -4,12 +4,32 @@ const game = canvas.getContext("2d");
 window.addEventListener("load", startGame);
 
 function startGame() {
-  game.fillRect(0, 50, 100, 100);
-  //   game.clearRect(50, 50, 50, 50);
-  // game.clearRect(0, 0, 50, 50);
+  let canvasSize;
 
-  game.font = "25px Verdana";
-  game.fillStyle = "red";
-  game.textAlign = "start";
-  game.fillText("Hello World", 50, 50);
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.75;
+  } else {
+    canvasSize = window.innerHeight * 0.75;
+  }
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  const elementsSize = canvasSize / 10;
+
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis["X"], elementsSize * i, elementsSize * i);
+  }
+
+  //   game.fillRect(0, 50, 100, 100);
+  //   //   game.clearRect(50, 50, 50, 50);
+  //   // game.clearRect(0, 0, 50, 50);
+
+  //   game.font = "25px Verdana";
+  //   game.fillStyle = "red";
+  //   game.textAlign = "start";
+  //   game.fillText("Hello World", 50, 50);
 }
