51590ea9cf2cd6830bb2d123f68c5c9904985d4e
doggbmx <manuelorticosta@gmail.com>
2023-07-18 21:30:50 -0400

    end class 2

---
+++ game.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load", startGame);
+
+function startGame() {
+  game.fillRect(0, 50, 100, 100);
+  //   game.clearRect(50, 50, 50, 50);
+  // game.clearRect(0, 0, 50, 50);
+
+  game.font = "25px Verdana";
+  game.fillStyle = "red";
+  game.textAlign = "start";
+  game.fillText("Hello World", 50, 50);
+}
